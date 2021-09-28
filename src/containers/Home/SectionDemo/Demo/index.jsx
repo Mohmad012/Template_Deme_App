43ca4f63c2c06efddf6ec36c90bd0743bba57786
Mohmad012 <mohmadailsia@gmail.com>
2021-09-28 16:06:54 +0200

    fix some of code

--- src/containers/Home/SectionDemo/Demo/index.jsx
+++ src/containers/Home/SectionDemo/Demo/index.jsx
@@ -1,13 +1,11 @@
-import { NavLink } from "react-router-dom";
-
 const Demo = ({img , path}) => {
 
   return (
       <div className="col-lg-4 col-md-6 col-sm-12">
           <div className="demo-item">
-              <NavLink to={path}><img src={img} alt="demo" className="img-responsive" /></NavLink>
+              <a href={path} target="_blank" rel="noopener noreferrer"><img src={img} alt="demo" className="img-responsive" /></a>
               <div className="preview-btn-wrapper text-center">
-                  <NavLink to={path} className="preview-demo">View demo <i className="fa fa-long-arrow-right"></i></NavLink>
+                  <a href={path} target="_blank" rel="noopener noreferrer" className="preview-demo">View demo <i className="fa fa-long-arrow-right"></i></a>
               </div>
           </div>
       </div>
